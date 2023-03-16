213fa8a2b3cd3b0b796f03885ffe3a28c179a052
dcortina <diego.cortina-garcia@capgemini.com>
2023-03-16 17:10:14 +0100

    added i18n and lang menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,60 @@
+import LanguageIcon from '@mui/icons-material/Language';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Button } from '@mui/material';
+import Drawer from '@mui/material/Drawer';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import classes from './Header.module.css'
+import classes from './Header.module.css';
+import EN from '../assets/uk.png'
+import ESP from '../assets/spain.png'
 
 const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleMenuOpen = () => {
+        setIsMenuOpen(true);
+    };
+
+    const handleMenuClose = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <header className={classes.header}>
             <nav>
                 <NavLink to="/" className={classes.link}>The Weather APP</NavLink>
             </nav>
-        </header>
+            <LanguageIcon
+                className={classes.menuBtn}
+                onClick={handleMenuOpen}
+                color="inherit"
+                aria-label="menu"
+            >
+                <MenuIcon />
+            </LanguageIcon>
+            <Drawer
+                className={classes.drawer}
+                onClose={handleMenuClose}
+                open={isMenuOpen}
+                anchor="right"
+            >
+                <Button
+                    endIcon={
+                        <img className={classes.img} src={EN} alt='English Language' />
+                    }
+                >
+                    English
+                </Button>
+                <Button
+                    endIcon={
+                        <img className={classes.img} src={ESP} alt='Spanish Language' />
+                    }
+                >
+                    Spanish
+                </Button>
+            </Drawer>
+        </header >
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
 import { BrowserRouter } from 'react-router-dom';
+import './i18n.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
